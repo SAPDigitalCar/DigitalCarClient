fbdff04bec914bdbbe3964bc7202c05dffd0b229
I319666 <rita.liu02@sap.com>
2019-12-05 19:11:08 +0800

    update

---
+++ pages/square/square.js
@@ -0,0 +1,92 @@
+// pages/square/square.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+    inputShowed: false,
+    inputVal: "",
+    cars: [{ "id": "1", "time": "5:30pm", "destination": "jinqiao" }, { "id": "2", "time": "4:30pm", "destination": "gaohang" }]
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  },
+
+
+  
+    showInput: function () {
+    this.setData({
+      inputShowed: true
+    });
+  },
+  hideInput: function () {
+    this.setData({
+      inputVal: "",
+      inputShowed: false
+    });
+  },
+  clearInput: function () {
+    this.setData({
+      inputVal: ""
+    });
+  },
+  inputTyping: function (e) {
+    this.setData({
+      inputVal: e.detail.value
+    });
+  }
+})
