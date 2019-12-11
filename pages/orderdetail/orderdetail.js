120e9733d696ea4a0f828ccbb12303e47b1e9848
I319666 <rita.liu02@sap.com>
2019-12-11 13:27:20 +0800

    gotodetail

--- pages/orderdetail/orderdetail.js
+++ pages/orderdetail/orderdetail.js
@@ -2,140 +2,118 @@
 const app = getApp()
 
 Page({
-
   /**
    * Page initial data
    */
   data: {
-    userInfo: {
-      "avatarUrl": "https://wx.qlogo.cn/mmopen/vi_32/gkiaIOSiaOLDBZTQCsRpxWbFbVnflJBJFhdXCq9MN7YnowdpOm9Jj2onYMuyMHZhhzhsRu6vLRo1cUljfaB1UjfA/132",
-      "nickName": "芒果",
-      "gender": 2,
-    },
-    people: [{
-      "avatarUrl": "https://wx.qlogo.cn/mmopen/vi_32/gkiaIOSiaOLDBZTQCsRpxWbFbVnflJBJFhdXCq9MN7YnowdpOm9Jj2onYMuyMHZhhzhsRu6vLRo1cUljfaB1UjfA/132",
-      "nickName": "芒果"
-    }, {
-        "avatarUrl": "https://wx.qlogo.cn/mmopen/vi_32/gkiaIOSiaOLDBZTQCsRpxWbFbVnflJBJFhdXCq9MN7YnowdpOm9Jj2onYMuyMHZhhzhsRu6vLRo1cUljfaB1UjfA/132",
-        "nickName": "芒果"
-      }, {
-        "avatarUrl": "https://wx.qlogo.cn/mmopen/vi_32/gkiaIOSiaOLDBZTQCsRpxWbFbVnflJBJFhdXCq9MN7YnowdpOm9Jj2onYMuyMHZhhzhsRu6vLRo1cUljfaB1UjfA/132",
-        "nickName": "芒果"
-      }],
-      ticketDetail:null,
-      ticket:null,
-      passengers:null,
-      driver:null,
-      licensePlateNumber: "",
-      carDescription:'',
-      seatCount:'',
-      nickName:'',
-      avatarUrl:'',
-      departureTime:'',
-      price:'',
-      departureAddresses:null
+    ticketDetail: null,
+    ticket: null,
+    passengers: null,
+    driver: null,
+    licensePlateNumber: "",
+    carDescription: '',
+    seatCount: '',
+    nickName: '',
+    avatarUrl: '',
+    departureTime: '',
+    price: '',
+    departureAddresses: null
   },
 
   /**
    * Lifecycle function--Called when page load
    */
-  onLoad: function (options) {
-    //  + options.id
-    let data = {
-      ticketDetail:null,
-      driver:null,
-      passengers:null,
-      ticket:null,
-      licensePlateNumber: "",
-      carDescription:'',
-      seatCount:'',
-      nickName:'',
-      avatarUrl:'',
-      departureTime:'',
-      price:'',
-      departureAddresses:''
-    };
-    wx.request({
-      url: app.globalData.host + '/ticket/detail/5',
-      header: {
-        openId: app.globalData.openId
-      },
-      method: 'get',
-      success: res => {
-        if (res.statusCode == 200 && res.data && res.data.data) {
-          data.ticketDetail=res.data.data
-          data.driver=res.data.data.driver
-          data.passengers = res.data.data.passengers
-          data.ticket = res.data.data.ticket
-          data.licensePlateNumber = data.driver.licensePlateNumber;
-          data.carDescription=data.driver.carDescription;
-          data.seatCount=data.driver.seatCount
-          data.nickName=data.driver.nickname
-          data.avatarUrl=data.driver.avatarUrl
-          data.departureTime=data.ticket.departureTime
-          data.price=data.ticket.price
-          data.departureAddresses=data.ticket.departureAddresses
-          this.setData(data)
-        }
-      },
-      fail: function (error) {
-        console.log(error);
-      }
-    });
-
+  onLoad: function(options) {
+    this.loadDetail(options.id);
   },
 
   /**
    * Lifecycle function--Called when page is initially rendered
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * Lifecycle function--Called when page show
    */
-  onShow: function () {
+  onShow: function() {
+
 
   },
 
   /**
    * Lifecycle function--Called when page hide
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * Lifecycle function--Called when page unload
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * Page event handler function--Called when user drop down
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * Called when page reach bottom
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * Called when user click on the top right corner to share
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   },
 
+  loadDetail: function(id) {
+    let that = this;
+    wx.request({
+      url: app.globalData.host + '/ticket/detail/' + id,
+      header: {
+        openId: app.globalData.openId
+      },
+      method: 'get',
+      success: res => {
+        if (res.statusCode == 200 && res.data && res.data.data) {
+          that.setData({
+            ticketDetail: res.data.data,
+            driver: res.data.data.driver,
+            passengers: res.data.data.passengers,
+            ticket: res.data.data.ticket,
+            licensePlateNumber: res.data.data.driver.licensePlateNumber,
+            carDescription: res.data.data.driver.carDescription,
+            seatCount: res.data.data.driver.seatCount,
+            nickName: res.data.data.driver.nickname,
+            avatarUrl: res.data.data.driver.avatarUrl,
+            departureTime: res.data.data.ticket.departureTime,
+            price: res.data.data.ticket.price,
+            departureAddresses: res.data.data.ticket.departureAddresses,
+          })
+
+        }
+      },
+      fail: function(error) {
+        console.log(error);
+      }
+    });
+  },
+
+
+
   tryJoin: function() {
-    let that=this
+    let that = this
     wx.showActionSheet({
       itemList: ['Join This Car'],
       success(res) {
@@ -161,7 +139,7 @@ Page({
               })
             }
           },
-          fail: function (error) {
+          fail: function(error) {
             wx.showToast({
               title: "Join Failed",
               duration: 2000,
--- pages/square/square.js
+++ pages/square/square.js
@@ -7,17 +7,21 @@ Page({
    * Page initial data
    */
   data: {
-   Dest: "2号线虹桥火车站",
-  multiArray: [['1号线', '2号线'], ['莘庄', '外环路', '莲花路', '锦江乐园', '上海南站', '漕宝路', '上海体育馆', '徐家汇', '衡山路', '常熟路', '陕西南路', '黄陂南路', '人民广场', '新闸路', '汉中路', '上海火车站', '中山北路', '延长路', '上海马戏城', '汶水路', '彭浦新村', '共康路', '通河新村', '呼兰路', '共富新村', '宝安公路', '友谊西路', '富锦路']],
-  multiIndex: [0, 0],
-  ticketsCount: 3,
-    tickets: [{ 'id': 1, 'time': '17:45', 'description': 'Big seats with free snacks', 'seats': 3, 'price': 20 }, { 'id': 2, 'time': '17:45', 'description': 'Big seats with free snacks', 'seats': 3, 'price': 20 }, { 'id': 3, 'time': '17:45', 'description': 'Big seats with free snacks', 'seats': 3, 'price': 20 }]
+    dest: "",
+    multiArray: [
+      ['1号线', '2号线'],
+      ['莘庄', '外环路', '莲花路', '锦江乐园', '上海南站', '漕宝路', '上海体育馆', '徐家汇', '衡山路', '常熟路', '陕西南路', '黄陂南路', '人民广场', '新闸路', '汉中路', '上海火车站', '中山北路', '延长路', '上海马戏城', '汶水路', '彭浦新村', '共康路', '通河新村', '呼兰路', '共富新村', '宝安公路', '友谊西路', '富锦路']
+    ],
+    multiIndex: [0, 0],
+    //   tickets: [{ 'id': 1, 'time': '17:45', 'description': 'Big seats with free snacks', 'seats': 3, 'price': 20 }, { 'id': 2, 'time': '17:45', 'description': 'Big seats with free snacks', 'seats': 3, 'price': 20 }, { 'id': 3, 'time': '17:45', 'description': 'Big seats with free snacks', 'seats': 3, 'price': 20 }]
+    tickets: []
   },
+
 
   /**
    * Lifecycle function--Called when page load
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     wx.showLoading({
       title: 'Loading',
       mask: true
@@ -29,7 +33,7 @@ Page({
       let userInfo = app.globalData.userInfo
       if (!userInfo) {
         this.goLogin(openId);
-      }else{
+      } else {
         this.getTicketsInfo();
       }
     }
@@ -48,12 +52,12 @@ Page({
               if (res.data.status == 200 && res.data.data) {
                 // openID保存在全局，保存手机号
                 app.globalData.openId = res.data.data;
-                if (!app.globalData.userInfo){
+                if (!app.globalData.userInfo) {
                   this.goLogin(res.data.data);
                 }
               }
             },
-            fail: function (error) {
+            fail: function(error) {
               console.log(error)
             }
           });
@@ -66,23 +70,23 @@ Page({
           })
         }
       },
-      fail: res => { },
-      complete: res => { },
+      fail: res => {},
+      complete: res => {},
     })
   },
   /**
    * Lifecycle function--Called when page is initially rendered
    */
-  onReady: function () {
-   
+  onReady: function() {
+
   },
 
   /**
    * Lifecycle function--Called when page show
    */
-  onShow: function () {
+  onShow: function() {
     let that = this;
-    if (app.globalData.userInfo){
+    if (app.globalData.userInfo) {
       that.getTicketsInfo();
     }
   },
@@ -90,51 +94,52 @@ Page({
   /**
    * Lifecycle function--Called when page hide
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * Lifecycle function--Called when page unload
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * Page event handler function--Called when user drop down
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * Called when page reach bottom
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * Called when user click on the top right corner to share
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   },
 
-  onPublishClick: function () {
+  onPublishClick: function() {
     wx.navigateTo({
       url: '../publish/publish'
     });
   },
 
-  goDetail: function () {
+  goDetail: function(e) {
+    let id = e.currentTarget.id;
     wx.navigateTo({
-      url: '../orderdetail/orderdetail'
+      url: '../orderdetail/orderdetail?id=' + id
     });
   },
 
-  goLogin: function (openid) {
+  goLogin: function(openid) {
     wx.request({
       url: app.globalData.host + '/user/info',
       header: {
@@ -143,13 +148,13 @@ Page({
       method: 'get',
       success: res => {
         if (res.statusCode == 200 && res.data && res.data.data) {
-          app.globalData.vo=res.data.data;
+          app.globalData.vo = res.data.data;
           if (!res.data.data.phone) {
             wx.navigateTo({
               url: '/pages/completeInfo/completeInfo'
             })
-          } else {//登陆成功 保存信息
-            app.globalData.userInfo=res.data.data
+          } else { //登陆成功 保存信息
+            app.globalData.userInfo = res.data.data
             this.getTicketsInfo();
           }
         } else {
@@ -160,7 +165,7 @@ Page({
           })
         }
       },
-      fail: function (error) {
+      fail: function(error) {
         console.log(error)
         wx.showToast({
           title: '登录失败，请检查网络稍后重试',
@@ -170,17 +175,19 @@ Page({
       }
     });
   },
-  bindMultiPickerChange: function (e) {
+  bindMultiPickerChange: function(e) {
     let that = this;
     console.log('picker发送选择改变，携带值为', e.detail.value)
     let newLine = that.data.multiArray[0][e.detail.value[0]];
-    let newStop = that.data.multiArray[1][e.detail.value[1]];     
-  
+    let newStop = that.data.multiArray[1][e.detail.value[1]];
+
     that.setData({
-      Dest: newLine + newStop
+      dest: newLine + newStop
     })
+
+    that.getTicketsInfoWithDest(newLine + newStop);
   },
-  bindMultiPickerColumnChange: function (e) {
+  bindMultiPickerColumnChange: function(e) {
     console.log('修改的列为', e.detail.column, '，值为', e.detail.value);
     var data = {
       multiArray: this.data.multiArray,
@@ -192,7 +199,7 @@ Page({
         switch (data.multiIndex[0]) {
           case 1:
             data.multiArray[1] = ['徐泾东', '虹桥火车站', '虹桥2号航站楼', '淞虹路', '北新泾', '威宁路', '娄山关路', '中山公园', '江苏路', '静安寺', '南京西路', '人民广场', '南京东路', '陆家嘴', '东昌路', '世纪大道', '上海科技馆', '世纪公园', '龙阳路', '张江高科', '金科路', '广兰路', '唐镇', '创新中路', '华夏东路', '川沙', '凌空路', '远东大道', '海天三路', '浦东国际机场'];
-          break;
+            break;
           case 0:
             data.multiArray[1] = ['莘庄', '外环路', '莲花路', '锦江乐园', '上海南站', '漕宝路', '上海体育馆', '徐家汇', '衡山路', '常熟路', '陕西南路', '黄陂南路', '人民广场', '新闸路', '汉中路', '上海火车站', '中山北路', '延长路', '上海马戏城', '汶水路', '彭浦新村', '共康路', '通河新村', '呼兰路', '共富新村', '宝安公路', '友谊西路', '富锦路'];
             break;
@@ -202,20 +209,22 @@ Page({
     }
     console.log(data.multiIndex);
     this.setData(data);
-  },  
-    
-  getTicketsInfo: function (){
+  },
+
+  getTicketsInfoWithDest: function(dest) {
     let that = this;
-    
     wx.request({
-      url: app.globalData.host + '/square/all',
+      url: app.globalData.host + '/square/select?destination=' + dest,
       header: {
         openId: app.globalData.openId
       },
       method: 'get',
       success: res => {
         wx.hideLoading();
-        if (res.statusCode == 200 ){
+        if (res.statusCode == 200) {
+          that.setData({
+            tickets: res.data.data.tickets
+          });
 
         } else {
           wx.showToast({
@@ -225,7 +234,42 @@ Page({
           })
         }
       },
-      fail: function (error) {
+      fail: function(error) {
+        console.log(error)
+        wx.showToast({
+          title: '登录失败，请检查网络稍后重试',
+          duration: 2000,
+          icon: 'none'
+        })
+      }
+    });
+  },
+  getTicketsInfo: function() {
+    let that = this;
+
+    wx.request({
+      url: app.globalData.host + '/square/all',
+      header: {
+        openId: app.globalData.openId
+      },
+      method: 'get',
+      success: res => {
+        wx.hideLoading();
+        if (res.statusCode == 200) {
+          that.setData({
+            dest: res.data.data.addresses[0].address,
+            tickets: res.data.data.tickets
+          });
+
+        } else {
+          wx.showToast({
+            title: '网络异常，请稍后重试',
+            duration: 10000,
+            icon: 'none'
+          })
+        }
+      },
+      fail: function(error) {
         console.log(error)
         wx.showToast({
           title: '登录失败，请检查网络稍后重试',
