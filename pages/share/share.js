9f3977cc100b6d085d5480a276f77b0cd05d22b1
I319666 <rita.liu02@sap.com>
2019-12-12 15:53:00 +0800

    add share page

--- pages/share/share.js
+++ pages/share/share.js
@@ -5,62 +5,88 @@ Page({
    * Page initial data
    */
   data: {
-
+    spots: [{
+        "time": '2019-12-13',
+        "price": "30",
+        "owner": 'Rita Liu'
+      },
+      {
+        "time": '2019-12-13',
+        "price": "20",
+        "owner": 'Min Huang'
+      },
+      {
+        "time": '2019-12-14',
+        "price": "25",
+        "owner": 'Tom Yang'
+      },
+      {
+        "time": '2019-12-14',
+        "price": "30",
+        "owner": 'Rita Liu'
+      },
+      {
+        "time": '2019-12-15',
+        "price": "30",
+        "owner": 'Rita Liu'
+      }
+    ],
+    spotsArray: ['SAP Labs', "Chang Tai' Square"]
   },
 
   /**
    * Lifecycle function--Called when page load
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * Lifecycle function--Called when page is initially rendered
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * Lifecycle function--Called when page show
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * Lifecycle function--Called when page hide
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * Lifecycle function--Called when page unload
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * Page event handler function--Called when user drop down
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * Called when page reach bottom
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * Called when user click on the top right corner to share
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
